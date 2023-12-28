63e304afcc9600c627a5d82eb9c267f59e4ab8b3
k0cx <dvo.2103@mail.ru>
2023-12-28 17:04:35 +0300

    check layout in dialog

--- main.py
+++ main.py
@@ -4,12 +4,64 @@ Config.set("graphics", "resizable", True)
 Config.set("graphics", "width", "350")
 Config.set("graphics", "height", "600")
 
+from kivy.lang import Builder
+
 from kivymd.app import MDApp
 
+# from kivymd.uix.toolbar.toolbar import MDFabBottomAppBarButton
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.pickers import MDDatePicker
-from datetime import datetime
-
 from kivymd.uix.list import TwoLineAvatarIconListItem, ILeftBodyTouch
 from kivymd.uix.selectioncontrol import MDCheckbox
+from kivymd.uix.toolbar import MDTopAppBar
+
+from datetime import datetime
+
+
+class DialogContent(MDBoxLayout):
+    """OPENS A DIALOG BOX THAT GETS THE TASK FROM THE USER"""
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # set the date_text label to today's date when user first opens dialog box
+        self.ids.date_text.text = str(datetime.now().strftime("%Y-%m-%d"))
+
+    def show_date_picker(self):
+        """Opens the date picker"""
+        date_dialog = MDDatePicker()
+        date_dialog.bind(on_save=self.on_save)
+        date_dialog.open()
+
+    def on_save(self, instance, value, date_range):
+        """This functions gets the date from the date picker and converts its it a
+        more friendly form then changes the date label on the dialog to that"""
+
+        date = value.strftime("%Y-%m-%d")
+        self.ids.date_text.text = str(date)
+
+
+class MainApp(MDApp):
+    task_list_dialog = None
+
+    def build(self):
+        # Setting theme
+        self.theme_cls.primary_palette = "DeepOrange"
+
+    def show_task_dialog(self):
+        if not self.task_list_dialog:
+            self.task_list_dialog = MDDialog(
+                title="Create Task",
+                type="custom",
+                content_cls=DialogContent(),
+            )
+
+        self.task_list_dialog.open()
+
+    def close_dialog(self, *args):
+        self.task_list_dialog.dismiss()
+
+
+if __name__ == "__main__":
+    app = MainApp()
+    app.run()
