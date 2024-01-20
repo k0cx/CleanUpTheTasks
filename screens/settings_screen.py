5eece92477515234ef46563da453c2bd9991c873
dvo2103 <dvo.2103@mail.ru>
2024-01-20 22:23:30 +0300

    me

--- screens/settings_screen.py
+++ screens/settings_screen.py
@@ -1,38 +1,31 @@
-# import bcrypt
 from pathlib import Path
 
-from cpuinfo import get_cpu_info  # py-cpuinfo
 import hashlib
+import json
+import platform
 from cryptocode import encrypt, decrypt
 from webdav3.client import Client  # webdavclient3
+from webdav3.exceptions import WebDavException  # webdavclient3
 
 from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 
 from kivymd.app import MDApp
+from kivymd.toast.kivytoast.kivytoast import toast
 from kivymd.uix.relativelayout import MDRelativeLayout
 
 
 class SettingsScreen(Screen):
     def generate_key_word(self):
-        # cpu_info = cpuinfo.get_cpu_info()
-
-        cpu_inf = (
-            "%s%s%s%s"
-            % (
-                get_cpu_info()["model"],
-                get_cpu_info()["family"],
-                "".join(get_cpu_info()["flags"]),
-                get_cpu_info()["arch"],
-            )
+        cpu_inf = str(
+            str(platform.system())
+            + str(platform.node())
+            + str(platform.version())
+            + str(platform.processor())
+            + str(platform.architecture())
+            + str(platform.machine())
         ).replace(" ", "")
-
-        # print(cpu_inf)
-        d = hashlib.sha1()
+        d = hashlib.sha256()
         d.update(cpu_inf.encode("utf-8", errors="ignore"))
-        # with open(
-        #     Path(__file__).parent.resolve() / "settings2.ini", "w", encoding="utf-8"
-        # ) as file:
-        #     file.write(d.hexdigest())
         return d.hexdigest()
 
     def encrypt_word(word: str, key_word: str) -> str:
@@ -42,34 +35,41 @@ class SettingsScreen(Screen):
         return decrypt(word, key_word)
 
     def crypt_input(self):
+        key_word = self.generate_key_word()
         sm = MDApp.get_running_app().root
-        # key_word = "The best password for Clean_up_the_Tasks"
-        login = sm.ids.settings_screen.ids.login_field.text
-        password = sm.ids.settings_screen.ids.password_container.ids.password_field.text
-        key_word = self.generate_key_word()
-        settings_ini = Path(__file__).parents[1] / "data/settings.ini"
+        login = sm.ids.settings_screen.ids.login_field
+        password = sm.ids.settings_screen.ids.password_container.ids.password_field
 
-        with open(settings_ini, "w", encoding="utf-8") as file:
-            file.write(f"{encrypt(login, key_word)}\n")
-            file.write(f"{encrypt(password, key_word)}\n")
-
-    def check_client(self):
-        settings_ini = Path(__file__).parents[1] / "data/settings.ini"
-        credentials = [
-            x.strip() for x in open(settings_ini, "r", encoding="utf-8") if x.strip()
-        ]
-        key_word = self.generate_key_word()
-        login = decrypt(credentials[0], key_word)
-        password = decrypt(credentials[1], key_word)
         data = {
             "webdav_hostname": "https://webdav.cloud.mail.ru",
-            "webdav_login": login,
-            "webdav_password": password,  # ubuntu
+            "webdav_login": encrypt(login.text, key_word),
+            "webdav_password": encrypt(password.text, key_word),
         }
-        client = Client(data)
+        settings_json = Path(__file__).parents[1] / "data/settings.json"
+        with open(settings_json, "w", encoding="utf-8") as file:
+            json.dump(data, file)
 
-        my_files = client.list()
-        print(my_files)
+    def check_client(self):
+        settings_json = Path(__file__).parents[1] / "data/settings.json"
+        with open(settings_json, "r", encoding="utf-8") as file:
+            data = json.load(file)
+
+        key_word = self.generate_key_word()
+        decrypt_data = {
+            "webdav_login": decrypt(data["webdav_login"], key_word),
+            "webdav_password": decrypt(data["webdav_password"], key_word),
+        }
+        data.update(decrypt_data)
+
+        try:
+            client = Client(data)
+            # client.verify = False
+            client.list()
+            toast(text="OK", duration=2)
+
+        except WebDavException as exception:
+            print(exception)
+            toast(text="FAULT", duration=5)
 
     def back_action(self):
         sm = MDApp.get_running_app().root
